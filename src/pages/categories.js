903754536e1c10b8eca128f496e870400f12f0b1
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-03-10 14:07:30 +0000

    Added category index

---
+++ src/pages/categories.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import kebabCase from "lodash/kebabCase"
+
+const Categories = ({ data: {
+  allMdx: { group },
+  site: {
+    siteMetadata: { title },
+  }
+}, location }) => {
+
+  return (
+    <Layout location={location} title={title}>
+      {group.map((node) => {
+        return (
+          <Link to={`/categories/${kebabCase(node.category)}/`}>
+            {node.category} ({node.totalCount})
+          </Link>
+        );
+      })}
+    </Layout>
+  )
+}
+
+export default Categories
+
+export const categoriesQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx {
+      group(field: frontmatter___categories) {
+        category: fieldValue
+        totalCount
+      }
+    }
+  }
+`
