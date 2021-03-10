45857cb66bb6eb3baf0f25848d46f2c15ec1ca6b
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-03-10 12:54:17 +0000

    Reinstated transform

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -11,6 +11,7 @@ const CodeBlock = ({children, className, live}) => {
       <div style={{marginTop: '40px'}}>
         <LiveProvider
           code={children.trim()}
+          transformCode={code => '/** @jsx mdx */' + code}
           scope={{ mdx }}
     _    >
           <LivePreview />
