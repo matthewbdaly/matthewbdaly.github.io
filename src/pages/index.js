b160f4c75fb011f5806673ec28e3adf4facb8d63
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-03-13 14:48:36 +0000

    Home page in place

---
+++ src/pages/index.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+
+const Index = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={`Home`}
+        description={`Home`}
+      />
+      <h4>Recent posts</h4>
+      {data.allMdx.edges.map(({ node }) => {
+        return (
+          <Link to={node.fields.path} key={node.fields.path}>
+            {node.frontmatter.title}
+          </Link>
+        );
+      })}
+    </Layout>
+  )
+}
+
+export default Index
+
+export const recentPostsQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx(
+      filter: {frontmatter: {layout: {eq: "post"}}}
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 5
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            path
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
