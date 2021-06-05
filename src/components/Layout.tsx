2cf39a64c810a8ce0e0c75c3fc1ef1e89d52cb4b
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-06-05 19:52:28 +0100

    Amended styling for header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,14 +5,18 @@ import Search from "./Search"
 const Layout = ({ title, children }) => {
     return (
         <div className="font-sans text-lg font-normal text-gray-900 bg-white dark:bg-gray-800 dark:text-gray-200">
+            <div className="w-full text-gray-200 bg-blue-800 shadow-lg h-36">
+                <div className="container p-4 mx-auto">
+                    <header>
+                        <h1 className="float-left font-serif text-4xl">
+                            <Link to={`/`}>{title}</Link>
+                        </h1>
+                        <Search />
+                    </header>
+                </div>
+            </div>
+            <div className="clear-both" />
             <div className="container p-4 mx-auto">
-                <header>
-                    <h1 className="float-left text-4xl font-serif">
-                        <Link to={`/`}>{title}</Link>
-                    </h1>
-                    <Search />
-                </header>
-                <div className="clear-both" />
                 <main>{children}</main>
             </div>
             <footer className="w-full h-24 p-4 text-center text-gray-200 align-middle bg-gray-900 clear-both">
