7c22628191c77d00af8db0de3be99dd9e63552f2
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-07-04 13:56:07 +0100

    Styled search results a bit

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -36,13 +36,13 @@ const SearchResults = ({ value }) => {
 			<div className="w-full">
 				<input type="search" id="search" placeholder="Search blog posts" name="s" onChange={updateSearch} className="float-left w-3/4 p-4 text-xl text-gray-900 border-2 border-gray-100 md:float-none" defaultValue={value} />
 				<button type="submit" className="float-right w-1/4 px-2 py-4 mx-0 border-2 md:float-none">Search</button>
-				<div className="w-full p-4 divide-y divide-gray-800 dark:divide-gray-200">
+				<div className="w-full divide-y divide-gray-800 dark:divide-gray-200 mt-2">
 					{results.map((result: Result) => (
-            <section key={result.id} className="w-full py-4 my-4">
-              <Link className="float-left w-full text-xl font-bold" to={result.path}>
+						<Link key={result.id} className="float-left w-full py-4 text-xl font-bold focus:outline-none focus:ring focus:border-blue-300" to={result.path}>
+            	<span key={result.id} className="block w-full px-2">
                 {result.title}
-              </Link>
-            </section>
+            	</span>
+            </Link>
 					))}
 				</div>
 			</div>
