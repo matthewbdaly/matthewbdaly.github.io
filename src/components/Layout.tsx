36050de81186a73f33415b84262c52a9d5746a92
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-07-07 18:45:32 +0100

    Made serif font the default

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ interface Props {
 const Layout = ({ title, children }: Props) => {
   return (
     <Fragment>
-      <div className="flex flex-col min-h-screen font-sans text-lg text-gray-800 bg-gray-200 dark:text-gray-100 dark:bg-gray-700 leading-6">
+      <div className="flex flex-col min-h-screen font-serif text-lg text-gray-800 bg-gray-200 dark:text-gray-100 dark:bg-gray-700 leading-6">
         <div className="w-full h-2 bg-gradient-to-r from-green-400 via-blue-500 to-purple-500" />
         <nav className="w-full p-4 mx-auto md:w-2/3 lowercase">
           <h1 className="block float-left py-4 text-4xl">
