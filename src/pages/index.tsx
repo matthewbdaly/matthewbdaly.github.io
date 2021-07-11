af14eb4c29fa8085f732942958e4bbb073f2e0f9
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-07-11 17:46:52 +0100

    Typed index properly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,16 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import RecentPosts from "../components/RecentPosts"
+import { PageData, SiteData } from "../Types"
 
-const Index = ({ data }): React.ReactElement => {
+interface Props {
+  data: {
+    site: SiteData;
+    mdx: PageData;
+  }
+}
+
+const Index = ({ data }: Props): React.ReactElement => {
   const siteTitle = data.site.siteMetadata.title
   const RecentPostsComponent = memo(RecentPosts)
 
