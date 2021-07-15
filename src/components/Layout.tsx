1d9f7ae2cac60a80e73d60dec8c1e17b6e120b83
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-07-16 00:50:05 +0100

    Added antialiasing

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,13 +11,13 @@ interface Props {
 const Layout = ({ title, children }: Props): React.ReactElement => {
   return (
     <Fragment>
-      <div className="flex flex-col min-h-screen font-serif text-lg text-gray-800 bg-gray-200 dark:text-gray-100 dark:bg-gray-700 leading-6">
+      <div className="flex flex-col min-h-screen font-serif text-lg antialiased text-gray-800 bg-gray-200 dark:text-gray-100 dark:bg-gray-700 leading-6">
         <div className="w-full h-2 bg-gradient-to-r from-green-400 via-blue-500 to-purple-500" />
-        <nav className="w-full p-4 mx-auto md:w-2/3 lowercase">
-          <h1 className="block float-none lg:float-left py-4 text-4xl text-center lg:text-left">
+        <nav className="w-full p-4 mx-auto lowercase md:w-2/3">
+          <h1 className="block float-none py-4 text-4xl text-center lg:float-left lg:text-left">
             <Link to={"/"}>{title}</Link>
           </h1>
-          <div className="lg:float-right py-2 md:block text-center lg:text-left">
+          <div className="py-2 text-center lg:float-right md:block lg:text-left">
             <NavLink to={"/blog/archives"} text="Archives" />
             <NavLink to={"/about"} text="About" />
             <NavLink to={"/uses"} text="Uses" />
