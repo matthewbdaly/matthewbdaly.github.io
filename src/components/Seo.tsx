d40c357796f2abdebbecef544f4cde8595f6ee65
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-08-01 17:25:04 +0100

    Removed Micropub endpoint for now

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -88,7 +88,6 @@ const SEO = ({ description, lang, meta, title }: Props): React.ReactElement => {
       <link href="/key.pub" rel="pgpkey" />
       <link href="https://webmention.io/matthewdaly.co.uk/xmlrpc" rel="pingback" />
       <link href="https://webmention.io/matthewdaly.co.uk/webmention" rel="webmention" />
-      <link rel="micropub" href="https://boiling-island-11588.herokuapp.com/micropub" />
       <link rel="authorization_endpoint" href="https://indieauth.com/auth" />
       <link rel="token_endpoint" href="https://tokens.indieauth.com/token" />
       <link rel="openid.delegate" href={ site.siteMetadata.siteUrl } />
