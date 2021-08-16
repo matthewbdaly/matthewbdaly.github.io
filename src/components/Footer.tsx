d77a2a64abfe9da82370b9ced4f3659c5584e6e7
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-08-16 21:55:57 +0100

    Amended some indentation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,11 +20,11 @@ const Footer = (): React.ReactElement => {
   return (
     <footer className="w-full p-4 text-gray-100 bg-gray-800">
       <div className="flex flex-row flex-wrap justify-center w-full p-2 mx-auto text-center md:w-2/3 box-border">
-          <Link className="w-1/3 md:w-1/5 my-2" to={"/posts/1"}>Blog</Link>
-        <Link className="w-1/3 md:w-1/5 my-2" to={"/about"}>About</Link>
-        <Link className="w-1/3 md:w-1/5 my-2" to={`https://twitter.com/${site.siteMetadata.social.twitter}`}>Twitter</Link>
-        <Link className="w-1/3 md:w-1/5 my-2" to={`https://github.com/${site.siteMetadata.social.github}`}>Github</Link>
-        <Link className="w-1/3 md:w-1/5 my-2" to={"/rss.xml"}>Feed</Link>
+        <Link className="w-1/3 my-2 md:w-1/5" to={"/posts/1"}>Blog</Link>
+        <Link className="w-1/3 my-2 md:w-1/5" to={"/about"}>About</Link>
+        <Link className="w-1/3 my-2 md:w-1/5" to={`https://twitter.com/${site.siteMetadata.social.twitter}`}>Twitter</Link>
+        <Link className="w-1/3 my-2 md:w-1/5" to={`https://github.com/${site.siteMetadata.social.github}`}>Github</Link>
+        <Link className="w-1/3 my-2 md:w-1/5" to={"/rss.xml"}>Feed</Link>
       </div>
       <div className="w-full p-4 mx-auto text-center md:w-2/3">
         <p>&copy; Matthew Daly {new Date().getFullYear()}</p>
