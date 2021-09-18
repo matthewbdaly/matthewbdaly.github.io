d0e2bebf1af0579a625a22e1dfd7f26fc4f26aa0
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-09-18 17:11:31 +0100

    Some progress with search

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, useState } from "react"
-import { useFlexSearch } from "react-use-flexsearch"
 import { Link, useStaticQuery, graphql } from "gatsby"
+import { Index } from "lunr"
 
 interface Result {
     id: string;
@@ -15,14 +15,24 @@ interface Props {
 const SearchResults = ({ value }: Props): React.ReactElement => {
   const data = useStaticQuery(graphql`
 		query {
-			localSearchPosts {
-				index
-				store
-			}
+			LunrIndex
 		}
 	`)
   const [query, setQuery] = useState(value)
-  const results = useFlexSearch(query, data.localSearchPosts.index, data.localSearchPosts.store)
+  const { store } = data.LunrIndex
+  const index = Index.load(data.LunrIndex.index)
+  let results = []
+  try {
+      results = index.search(query).map(({ ref }) => {
+          return {
+              path: ref,
+              ...store[ref],
+          }
+      })
+      console.log(results)
+  } catch (error) {
+      console.log(error)
+  }
 
   const updateSearch = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.currentTarget.value.length > 2) {
@@ -37,7 +47,7 @@ const SearchResults = ({ value }: Props): React.ReactElement => {
       <div className="w-full">
         <input type="search" id="search" placeholder="Search blog posts" name="s" onChange={updateSearch} className="float-left w-3/4 p-4 text-xl text-gray-900 border-2 border-gray-100 md:float-none" defaultValue={value} />
         <button type="submit" className="float-right w-1/4 px-2 py-4 mx-0 border-2 md:float-none">Search</button>
-        <div className="w-full divide-y divide-gray-800 dark:divide-gray-200 mt-2">
+        <div className="w-full mt-2 divide-y divide-gray-800 dark:divide-gray-200">
           {results.map((result: Result) => (
             <Link key={result.id} className="float-left w-full py-4 text-xl font-bold focus:outline-none focus:ring focus:border-blue-300" to={result.path}>
               <span key={result.id} className="block w-full px-2">
