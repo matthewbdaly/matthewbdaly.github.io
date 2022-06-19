ad072f512105785b1e92a6f511be5feedab95dae
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2022-06-19 18:11:02 +0100

    Indented notice

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -6,12 +6,12 @@ interface Props {
 }
 
 const Notice = (props: Props): React.ReactElement => {
-    return (
-        <div className="bg-red-200 b-2 p-4 my-4 rounded-lg shadow-lg">
-            <h2 className="text-xl font-bold">{props.title}</h2>
-            {props.children}
-        </div>
-    )
+  return (
+    <div className="bg-red-200 b-2 p-4 my-4 rounded-lg shadow-lg">
+      <h2 className="text-xl font-bold">{props.title}</h2>
+      {props.children}
+    </div>
+  )
 }
 
 export default Notice
