f280f6c2eb2ecb6e92bcfd60d2d978d9996060ff
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2022-06-19 18:16:22 +0100

    Cleaned up notice component a bit

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React, { ReactElement, ReactNode } from "react"
 
 interface Props {
-    children: React.ReactNode;
+    children: ReactNode;
     title: string;
 }
 
-const Notice = (props: Props): React.ReactElement => {
+const Notice = (props: Props): ReactElement => {
   return (
     <div className="bg-red-200 b-2 p-4 my-4 rounded-lg shadow-lg">
       <h2 className="text-xl font-bold">{props.title}</h2>
